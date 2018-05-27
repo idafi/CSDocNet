437fe9f017d01af91ab03aaf2e38c1cb4663893e
idafi <none>
2018-05-27 14:11:03 -0500

    let's not sort member types just yet

--- src/NADS/NADS.Comments/AssemblyComments.cs
+++ src/NADS/NADS.Comments/AssemblyComments.cs
@@ -6,31 +6,15 @@ namespace NADS.Comments
     public readonly struct AssemblyComments
     {
         public readonly string Name;
+        public readonly IReadOnlyList<MemberComments> Members;
 
-        public readonly IReadOnlyDictionary<string, MemberComments> Types;
-        public readonly IReadOnlyDictionary<string, MemberComments> Fields;
-        public readonly IReadOnlyDictionary<string, MemberComments> Properties;
-        public readonly IReadOnlyDictionary<string, MemberComments> Methods;
-        public readonly IReadOnlyDictionary<string, MemberComments> Events;
-
-        public AssemblyComments(string name,
-            IReadOnlyDictionary<string, MemberComments> types,
-            IReadOnlyDictionary<string, MemberComments> fields,
-            IReadOnlyDictionary<string, MemberComments> properties,
-            IReadOnlyDictionary<string, MemberComments> methods,
-            IReadOnlyDictionary<string, MemberComments> events
-        )
+        public AssemblyComments(string name, IReadOnlyList<MemberComments> members)
         {
             Name = name ?? "";
-
-            Types = types ?? Empty<string, MemberComments>.EmptyDict;
-            Fields = fields ?? Empty<string, MemberComments>.EmptyDict;
-            Properties = properties ?? Empty<string, MemberComments>.EmptyDict;
-            Methods = methods ?? Empty<string, MemberComments>.EmptyDict;
-            Events = events ?? Empty<string, MemberComments>.EmptyDict;
+            Members = members ?? Empty<MemberComments>.EmptyList;
         }
 
         public static AssemblyComments Empty
-            => new AssemblyComments(null, null, null, null, null, null);
+            => new AssemblyComments(null, null);
     }
 }
