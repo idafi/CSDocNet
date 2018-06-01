6572b67b353d6303239e060f45461f539af8cd85
idafi <none>
2018-06-01 11:38:17 -0500

    implement method name generation. finally

--- src/NADS/NADS.Reflection/MemberCommentNameGenerator.cs
+++ src/NADS/NADS.Reflection/MemberCommentNameGenerator.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using NADS.Debug;
 
 namespace NADS.Reflection
@@ -10,14 +12,14 @@ namespace NADS.Reflection
         {
             Check.Ref(type);
 
-            return $"T:{FormatTypeName(type)}";
+            return $"T:{FormatTypeName(type, false)}";
         }
 
         public string GenerateFieldName(FieldInfo field)
         {
             Check.Ref(field);
             
-            string type = FormatTypeName(field.DeclaringType);
+            string type = FormatTypeName(field.DeclaringType, false);
             return $"F:{type}.{field.Name}";
         }
 
@@ -28,7 +30,15 @@ namespace NADS.Reflection
 
         public string GenerateMethodName(MethodInfo method)
         {
-            throw new NotImplementedException();
+            Check.Ref(method);
+
+            int typeParamCt = method.GetGenericArguments().Length;
+
+            string typeName = FormatTypeName(method.DeclaringType, false);
+            string typeParams = (typeParamCt > 0) ? $"``{typeParamCt}" : "";
+            string paramList = FormatParameterList(method.GetParameters());
+            
+            return $"M:{typeName}.{method.Name}{typeParams}{paramList}";
         }
 
         public string GenerateMethodName(ConstructorInfo ctor)
@@ -41,16 +51,103 @@ namespace NADS.Reflection
             throw new NotImplementedException();
         }
 
-        string FormatTypeName(Type type)
+        string FormatTypeName(Type type, bool listTypeParams)
         {
             Assert.Ref(type);
 
             string name = type.ToString();
-            name = name.Replace('+', '.');      // convert nested type syntax
-            if(type.ContainsGenericParameters)  // strip typeparam names, if present
-            { name = name.Substring(0, name.IndexOf('[')); }
+            name = name.Replace('+', '.');
+
+            int typeParamList = name.IndexOf('[');
+            if(typeParamList > -1)
+            { name = name.Substring(0, typeParamList); }
+
+            if(listTypeParams)
+            {
+                Span<Type> typeParams = type.GetGenericArguments();
+                MatchCollection tpCtMatches = Regex.Matches(name, @"\`(\d+)");
+                
+                for(int i = 0; i < tpCtMatches.Count; i++)
+                {
+                    Match match = tpCtMatches[tpCtMatches.Count - 1 - i];
+                    int typeParamCount = int.Parse(match.Groups[1].Value);
+
+                    Span<Type> usedParams = typeParams.Slice(typeParams.Length - typeParamCount);
+                    string list = '{' + FormatTypeList(usedParams) + '}';
+
+                    name = name.Remove(match.Index, match.Length);
+                    name = name.Insert(match.Index, list);
+
+                    typeParams = typeParams.Slice(0, typeParams.Length - typeParamCount);
+                }
+            }
 
             return name;
+        }
+
+        string FormatParameterList(ParameterInfo[] parameters)
+        {
+            if(parameters != null && parameters.Length > 0)
+            {
+                Type[] types =
+                    (from ParameterInfo param in parameters
+                    select param.ParameterType).ToArray();
+
+                return $"({FormatTypeList(types)})";
+            }
+
+            return "";
+        }
+
+        string FormatTypeList(Span<Type> types)
+        {
+            if(types == null || types.Length <= 0)
+            { return ""; }
+
+            string[] paramNames = new string[types.Length];
+            for(int i = 0; i < types.Length; i++)
+            {
+                ref string name = ref paramNames[i];
+                Type type = types[i];
+
+                if(type.IsByRef)
+                {
+                    name = "@";
+                    type = type.GetElementType();
+                }
+
+                while(type.IsArray)
+                {
+                    int dimCount = type.GetArrayRank();
+
+                    if(dimCount > 1)
+                    {
+                        string[] prefixes = new string[dimCount];
+                        for(int d = 0; d < dimCount; d++)
+                        { prefixes[d] = "0:"; }
+
+                        name = $"[{string.Join(",", prefixes)}]" + name;
+                    }
+                    else
+                    { name = "[]" + name; }
+
+                    type = type.GetElementType();
+                }
+
+                if(type.IsGenericParameter)
+                {
+                    name = type.GenericParameterPosition + name;
+
+                    if(type.DeclaringMethod != null)
+                    { name = "``" + name; }
+                    else
+                    { name = '`' + name; }
+                }
+                else
+                { name = FormatTypeName(type, true) + name; }
+            }
+
+            return string.Join(",", paramNames);
         }
     }
 }
