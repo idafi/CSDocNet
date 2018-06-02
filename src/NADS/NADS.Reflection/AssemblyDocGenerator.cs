ccc86c89f39f038300778058996c93f8ad2ac32e
idafi <none>
2018-06-02 16:53:47 -0500

    implement + test access & modifier reading from Type

--- src/NADS/NADS.Reflection/AssemblyDocGenerator.cs
+++ src/NADS/NADS.Reflection/AssemblyDocGenerator.cs
@@ -65,5 +65,48 @@ namespace NADS.Reflection
         {
             throw new NotImplementedException();
         }
+
+        public AccessModifier GetTypeAccess(Type type)
+        {
+            if(type.IsPublic || type.IsNestedPublic)
+            { return AccessModifier.Public; }
+            else if(!type.IsNested || type.IsNestedAssembly)
+            { return AccessModifier.Internal; }
+            else if(type.IsNestedFamORAssem)
+            { return AccessModifier.ProtectedInternal; }
+            else if(type.IsNestedFamily)
+            { return AccessModifier.Protected; }
+            else if(type.IsNestedFamANDAssem)
+            { return AccessModifier.PrivateProtected; }
+            else if(type.IsNestedPrivate)
+            { return AccessModifier.Private; }
+            else
+            { throw new NotSupportedException($"Unknown access modifier on type '{type.Name}'"); }
+        }
+
+        public Modifier GetTypeModifiers(Type type)
+        {
+            Modifier mod = Modifier.None;
+
+            if(type.IsAbstract && type.IsSealed)
+            { mod |= Modifier.Static; }
+            else if(type.IsAbstract)
+            { mod |= Modifier.Abstract; }
+            else if(type.IsSealed && !type.IsValueType)
+            { mod |= Modifier.Sealed; }
+
+            // IsReadOnlyAttribute is private; thus, the hackiest hack on the planet of hacks
+            string readonlyAttrName = "System.Runtime.CompilerServices.IsReadOnlyAttribute";
+            foreach(var attr in type.CustomAttributes)
+            {
+                if(attr.AttributeType.FullName == readonlyAttrName)
+                {
+                    mod |= Modifier.Readonly;
+                    break;
+                }
+            }
+
+            return mod;
+        }
     }
 }
---
+++ src/Test/Test.NADS/NADS.Reflection/AssemblyDocGeneratorTest.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Reflection;
+using NUnit.Framework;
+using NADS.TestDoc;
+
+namespace NADS.Reflection
+{
+    internal class InternalClass { }
+    class DefaultAccessClass { }
+
+    [TestFixture]
+    public class AssemblyDocGeneratorTest
+    {
+        internal class InternalNestedClass { }
+        protected internal class ProtectedInternalClass { }
+        protected class ProtectedClass { }
+        private protected class PrivateProtectedClass { }
+        private class PrivateClass { }
+        class DefaultAccessNestedClass { }
+
+        class NormalClass { }
+        sealed class SealedClass { }
+        abstract class AbstractClass { }
+        static class StaticClass { }
+
+        struct NormalStruct { }
+        readonly struct ReadonlyStruct { }
+
+        AssemblyDocGenerator gen;
+
+        [SetUp]
+        public void SetUp()
+        {
+            gen = new AssemblyDocGenerator();
+        }
+
+        [Test]
+        public void TestGetTypeAccess()
+        {
+            Assert.AreEqual(AccessModifier.Public, gen.GetTypeAccess(typeof(TestClass)));
+            Assert.AreEqual(AccessModifier.Internal, gen.GetTypeAccess(typeof(InternalClass)));
+            Assert.AreEqual(AccessModifier.ProtectedInternal, gen.GetTypeAccess(typeof(ProtectedInternalClass)));
+            Assert.AreEqual(AccessModifier.Protected, gen.GetTypeAccess(typeof(ProtectedClass)));
+            Assert.AreEqual(AccessModifier.PrivateProtected, gen.GetTypeAccess(typeof(PrivateProtectedClass)));
+            Assert.AreEqual(AccessModifier.Private, gen.GetTypeAccess(typeof(PrivateClass)));
+            Assert.AreEqual(AccessModifier.Internal, gen.GetTypeAccess(typeof(DefaultAccessClass)));
+
+            Assert.AreEqual(AccessModifier.Public, gen.GetTypeAccess(typeof(TestClass.NestedClass)));
+            Assert.AreEqual(AccessModifier.Internal, gen.GetTypeAccess(typeof(InternalNestedClass)));
+            Assert.AreEqual(AccessModifier.Private, gen.GetTypeAccess(typeof(DefaultAccessNestedClass)));
+        }
+
+        [Test]
+        public void TestGetTypeModifiers()
+        {
+            Assert.AreEqual(Modifier.None, gen.GetTypeModifiers(typeof(NormalClass)));
+            Assert.AreEqual(Modifier.Sealed, gen.GetTypeModifiers(typeof(SealedClass)));
+            Assert.AreEqual(Modifier.Abstract, gen.GetTypeModifiers(typeof(AbstractClass)));
+            Assert.AreEqual(Modifier.Static, gen.GetTypeModifiers(typeof(StaticClass)));
+
+            Assert.AreEqual(Modifier.None, gen.GetTypeModifiers(typeof(NormalStruct)));
+            Assert.AreEqual(Modifier.Readonly, gen.GetTypeModifiers(typeof(ReadonlyStruct)));
+        }
+    }
+}
