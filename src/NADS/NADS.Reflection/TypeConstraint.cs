a968f90d9cf05547650cbb7ee42718b4157ae80a
idafi <none>
2018-06-03 12:49:42 -0500

    add helper builders for type constraint struct

--- src/NADS/NADS.Reflection/TypeConstraint.cs
+++ src/NADS/NADS.Reflection/TypeConstraint.cs
@@ -5,15 +5,29 @@ namespace NADS.Reflection
         public readonly ConstraintType Constraint;
         
         public readonly MemberRef ConstrainedType;
-        public readonly int ConstrainedTypeParam;
+        public readonly int ConstrainedTypeParamPosition;
 
-        public TypeConstraint(ConstraintType constraint, in MemberRef constrainedType,
-            int constrainedTypeParam)
+        TypeConstraint(ConstraintType constraint, in MemberRef constrainedType,
+            int constrainedTypeParamPosition)
         {
             Constraint = constraint;
             
             ConstrainedType = constrainedType;
-            ConstrainedTypeParam = constrainedTypeParam;
+            ConstrainedTypeParamPosition = constrainedTypeParamPosition;
         }
+
+        public static TypeConstraint Struct
+            => new TypeConstraint(ConstraintType.Struct, default, default);
+        public static TypeConstraint Class
+            => new TypeConstraint(ConstraintType.Class, default, default);
+        public static TypeConstraint Ctor
+            => new TypeConstraint(ConstraintType.Ctor, default, default);
+        public static TypeConstraint Unmanaged
+            => new TypeConstraint(ConstraintType.Unmanaged, default, default);
+
+        public static TypeConstraint Type(in MemberRef constrainedType)
+            => new TypeConstraint(ConstraintType.Type, constrainedType, default);
+        public static TypeConstraint TypeParam(int typeParamPosition)
+            => new TypeConstraint(ConstraintType.TypeParam, default, typeParamPosition);
     }
 }
