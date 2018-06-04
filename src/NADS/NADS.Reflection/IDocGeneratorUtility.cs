44d56e1529074338119f5fba5452238b3c526757
idafi <none>
2018-06-04 12:41:45 -0500

    add attribute generation to doc utility

--- src/NADS/NADS.Reflection/IDocGeneratorUtility.cs
+++ src/NADS/NADS.Reflection/IDocGeneratorUtility.cs
@@ -6,8 +6,9 @@ namespace NADS.Reflection
 {
     public interface IDocGeneratorUtility
     {
+        IReadOnlyList<MemberRef> GenerateAttributes(MemberInfo memberInfo);
+
         MemberRef MakeMemberRef(MemberInfo member);
-        
         ParamModifier GetGenericParamModifier(GenericParameterAttributes attributes);
         IReadOnlyList<TypeConstraint> GetTypeParamConstraints(Type typeParam);
     }
