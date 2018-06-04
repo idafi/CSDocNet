f0a6c45a7a6f8e895aa928b4e30cc49fc65ae524
idafi <none>
2018-06-04 15:48:42 -0500

    property doc doesn't require field doc

--- src/NADS/NADS.Reflection/Data/PropertyDoc.cs
+++ src/NADS/NADS.Reflection/Data/PropertyDoc.cs
@@ -3,17 +3,15 @@ namespace NADS.Reflection.Data
     public readonly struct PropertyDoc
     {
         public readonly MemberDoc Member;
-        public readonly FieldDoc Field;
 
         public readonly bool HasGetAccessor;
         public readonly bool HasSetAccessor;
 
-        public PropertyDoc(in MemberDoc member, in FieldDoc field,
+        public PropertyDoc(in MemberDoc member,
             bool hasGetAccessor, bool hasSetAccessor)
         {
             Member = member;
-            Field = field;
-
+            
             HasGetAccessor = hasGetAccessor;
             HasSetAccessor = hasSetAccessor;
         }
