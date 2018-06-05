3bcaefe04f91e47146ea52ebd5414f73b4748bcb
idafi <none>
2018-06-05 14:50:05 -0500

    add array data to member ref

--- src/NADS/NADS.Reflection/Data/MemberRef.cs
+++ src/NADS/NADS.Reflection/Data/MemberRef.cs
@@ -1,14 +1,20 @@
+using System.Collections.Generic;
+
 namespace NADS.Reflection.Data
 {
     public readonly struct MemberRef
     {
         public readonly MemberRefType Type;
         public readonly string Name;
+
+        public readonly IReadOnlyList<int> ArrayDimensions;
         
-        public MemberRef(MemberRefType type, string name)
+        public MemberRef(MemberRefType type, string name, IReadOnlyList<int> arrayDimensions)
         {
             Type = type;
             Name = name ?? "";
+
+            ArrayDimensions = arrayDimensions;
         }
     }
 }
