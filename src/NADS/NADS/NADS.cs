dd0699e421580635a9481c657c1995cabc0dcccf
idafi <none>
2018-06-11 11:32:06 -0500

    add quick hack test for reflection to entry point

--- src/NADS/NADS/NADS.cs
+++ src/NADS/NADS/NADS.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics;
+using System.Reflection;
 using System.IO;
 using NADS.Comments;
 using NADS.Logging;
+using NADS.Reflection.Generation;
 
 namespace NADS
 {
@@ -14,6 +16,7 @@ namespace NADS
             if(args != null)
             {
                 var parser = new AssemblyCommentParser();
+                var gen = MakeAssemblyDocGen();
 
                 Stopwatch sw = new Stopwatch();
 
@@ -26,11 +29,24 @@ namespace NADS
                         using(FileStream stream = new FileStream(arg, FileMode.Open))
                         {
                             var comments = parser.Parse(stream);
+
+                            sw.Stop();
                             Log.Note($"parsed {comments.Members.Count} members from {comments.Name} assembly in {sw.ElapsedMilliseconds} ms");
+                            
+                            sw.Restart();
+                            string assName = $"{comments.Name}.dll";
+                            assName = Path.GetFullPath(assName);
+                            if(File.Exists(assName))
+                            {
+                                Assembly ass = Assembly.LoadFile(assName);
+                                var doc = gen.GenerateAssemblyDoc(ass);
+
+                                sw.Stop();
+                                Log.Note($"generated reflected doc in {sw.ElapsedMilliseconds} ms");
+                            }
                         }
                     }
 
-                    sw.Stop();
                     sw.Reset();
                 }
             }
@@ -39,6 +55,26 @@ namespace NADS
         static void InitLog()
         {
             Log.AddLogger(new ConsoleLogger(), LogLevel.Debug);
+        }
+
+        static AssemblyDocGenerator MakeAssemblyDocGen()
+        {
+            var docUtility = new DocGeneratorUtility();
+            var idGen = new CommentIDGenerator();
+            var typeUtility = new TypeDocUtility(docUtility);
+            var methodUtility = new MethodBaseUtility(docUtility, idGen);
+            var methodGen = new MethodDocGenerator(docUtility, methodUtility);
+
+            return new AssemblyDocGenerator(
+                new ClassDocGenerator(docUtility, typeUtility, idGen),
+                new EnumDocGenerator(docUtility, typeUtility, idGen),
+                new EventDocGenerator(docUtility, methodUtility, idGen),
+                new FieldDocGenerator(docUtility, idGen),
+                new PropertyDocGenerator(docUtility, methodUtility, idGen),
+                new ConstructorDocGenerator(methodUtility),
+                new OperatorDocGenerator(methodGen),
+                methodGen
+            );
         }
     }
 }
